afd07b19168a5d3c6187d5e4bcdd91cd9f62157d
thanhhoan062 <Ace0984423462>
2022-08-12 21:29:36 +0700

    #commit kết nối mongodb

---
+++ client/src/assets/index.js
@@ -0,0 +1,10 @@
+const assets = {
+    images: {
+        logoDark: require('.images/logo-dark.png'),
+        appIcon: require('.images/favicon.png')
+    },
+    color: {
+        secondary: '#292929'
+    }
+}
+export default assets;
