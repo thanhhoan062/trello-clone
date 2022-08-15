24af821439b1d4984ad9aba859890debe765c4ea
thanhhoan062 <Ace0984423462>
2022-08-15 22:05:56 +0700

    #commit signup + login

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, TextField } from '@mui/material'
+import LoadingButton from '@mui/lab/LoadingButton'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const [loading, setLoading] = useState(false)
+    const [usernameErrText, setUsernameErrText] = useState('')
+    const [passwordErrText, setPasswordErrText] = useState('')
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>Login</div>
+        <>
+            <Box
+                component='form'
+                sx={{ mt: 1 }}
+                onSubmit={handleSubmit}
+                noValidate
+            >
+                <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='username'
+                    label='Username'
+                    name='username'
+                    disabled={loading}
+                    error={usernameErrText !== ''}
+                    helperText={usernameErrText}
+                />
+                <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='password'
+                    label='Password'
+                    name='password'
+                    type='password'
+                    disabled={loading}
+                    error={passwordErrText !== ''}
+                    helperText={passwordErrText}
+                />
+                <LoadingButton
+                    sx={{ mt: 3, mb: 2 }}
+                    variant='outlined'
+                    fullWidth
+                    color='success'
+                    type='submit'
+                    loading={loading}
+                >
+                    Login
+                </LoadingButton>
+                Don't have an account?
+                <Button
+                    component={Link}
+                    to='/signup'
+                    sx={{ textTransform: 'none', marginBottom: '1px' }}
+                >
+                    Signup
+                </Button>
+            </Box>
+        </>
     )
 }
 
